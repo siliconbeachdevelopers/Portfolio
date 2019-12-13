8e109420d547aebf3522c63fb0f2ce3575ba8cb9
Adam Silver <adamsilver.ca@gmail.com>
2019-12-13 11:10:11 -0800

    Updates

---
+++ precache-manifest.c12256af9d5da26a0c9db48d66b28653.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac552d352df13292893b5091711bf227",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "de026888184799ad608b",
+    "url": "/Portfolio/static/css/main.67ae86c3.chunk.css"
+  },
+  {
+    "revision": "c9992d1010b22bfbe0a4",
+    "url": "/Portfolio/static/js/2.88859a3d.chunk.js"
+  },
+  {
+    "revision": "de026888184799ad608b",
+    "url": "/Portfolio/static/js/main.b7080940.chunk.js"
+  },
+  {
+    "revision": "4c2512ee8df4197b60d3",
+    "url": "/Portfolio/static/js/runtime-main.373122be.js"
+  }
+]);
