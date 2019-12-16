08b5e217ec0c77810907e8e0916d0a029b4523f8
Adam Silver <adamsilver.ca@gmail.com>
2019-12-15 22:31:01 -0800

    get

--- src/components/Testimonials/testimonials.js
+++ src/components/Testimonials/testimonials.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-export default class Header extends Component {
-  render() {
-    return (
-      <React.Fragment>
-      {/*generated code*/}
-      <header id="home">
-        ....
-      </header>
-      </React.Fragment>
-    );
-  }
-}
+// import React, { Component } from 'react';
+// export default class Header extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//       {/*generated code*/}
+//       <header id="home">
+//         ....
+//       </header>
+//       </React.Fragment>
+//     );
+//   }
+// }
