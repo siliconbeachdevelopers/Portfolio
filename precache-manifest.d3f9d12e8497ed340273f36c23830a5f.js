88f81b2347996e057a5df3396b3205322348ca6e
Adam Silver <adamsilver.ca@gmail.com>
2020-03-25 23:58:47 -0700

    Updates

---
+++ precache-manifest.d3f9d12e8497ed340273f36c23830a5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb6657967bde27059f331906f0d3b8a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "595b4e0ac5637c8e2974",
+    "url": "/Portfolio/static/css/main.edff9d12.chunk.css"
+  },
+  {
+    "revision": "6ed61a5b1f0ec811f7b5",
+    "url": "/Portfolio/static/js/2.5195218e.chunk.js"
+  },
+  {
+    "revision": "595b4e0ac5637c8e2974",
+    "url": "/Portfolio/static/js/main.5ffd06fd.chunk.js"
+  },
+  {
+    "revision": "4c2512ee8df4197b60d3",
+    "url": "/Portfolio/static/js/runtime-main.373122be.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.3fdeca6cba747855b8a1544d1ad725d9.js"
+  "/Portfolio/precache-manifest.d3f9d12e8497ed340273f36c23830a5f.js"
 );
 
 self.addEventListener('message', (event) => {
